3fa5d2f6a45e1fed5e161c2609ecb880d13eb31e
Victor <dibiavc@us.ibm.com>
2017-10-16 20:19:56 -0400

    update ffplay volume support

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ module.exports.prototype.play = function(options) {
 
     switch (this.options.player) {
         case "aplay":
-            this.process = spawn('aplay', [this.options.filename, '--device', this.options.device]);
+            this.process = spawn('aplay', [this.options.filename, '-D', this.options.device]);
             break;
         case "afplay":
             this.process = spawn('afplay', [this.options.filename, '-v', this.options.gain]);
