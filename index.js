aed409f5514ef2362258c5a6fa89abde889d2a9d
Matthew Villeneuve <mvilleneuve@linkedin.com>
2018-09-15 06:24:58 -0700

    add emit "play" event

--- index.js
+++ index.js
@@ -59,15 +59,19 @@ module.exports.prototype.play = function(options) {
     switch (this.options.player) {
         case "aplay":
             this.process = spawn('aplay', [this.options.filename, '-D', this.options.device]);
+            self.emit('play');
             break;
         case "afplay":
             this.process = spawn('afplay', [this.options.filename, '-v', this.options.gain]);
+            self.emit('play');
             break;
         case "mpg321":
             this.process = spawn('mpg321', [this.options.filename, '-g', this.options.gain, '-a', this.options.device]);
+            self.emit('play');
             break;
         case "mpg123":
             this.process = spawn('mpg123', [this.options.filename, '-g', this.options.gain, '-a', this.options.device]);
+            self.emit('play');
             break;
         case "ffplay":
             var vol = "'volume=" + this.options.gain / 100 + "'";
@@ -75,6 +79,7 @@ module.exports.prototype.play = function(options) {
             this.process = spawn('ffplay', [this.options.filename, '-nodisp', '-autoexit', '-volume', this.options.gain], {
                 stdio: 'ignore'
             }); //, '-volume', this.options.gain, '-a', this.options.device, '-nodisp']);
+            self.emit('play');
             break;
     }
     this.process.on('exit', function(code, sig) {
